6da482c50ec44764a4ce0a620a9cb134c75201d3
Darrel <darrel124568@gmail.com>
2026-07-30 16:18:42 +0300

    added an entries endpoint to get journal entries for a user with pagination

--- server/app.py
+++ server/app.py
@@ -6,6 +6,12 @@ from models import User, UserSchema, JournalEntry, JournalEntrySchema
 
 api = Api(app=app)
 
+@app.before_request
+def before_request():
+    accepted = ["signup", "login"]
+    if request.endpoint not in accepted and (not session.get("user_id")):
+        return {}, 401
+    
 #=============
 #SIGN-UP ROUTE
 #=============
@@ -47,25 +53,33 @@ class Login(Resource):
 #=============
 class CheckSession(Resource):
     def get(self):
-        if session.get('user_id'):
-            user = User.query.get(session["user_id"])
-            return UserSchema().dump(user), 200
-        return {}, 401
+        user = User.query.get(session["user_id"])
+        return UserSchema().dump(user), 200
 #=============
 #LOGOUT ROUTE
 #=============
 class Logout(Resource):
     def delete(self):
-        if session.get('user_id'):
-            session.pop("user_id")
-            return {}, 201
-        return {}, 401
+        session.pop("user_id")
+        return {}, 201
 
+#===========================================
+#JOURNAL ENTRIES INDEX ROUTE WITH PAGINATION
+#===========================================
+class Entries(Resource):
+    def get(self):
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 5, type=int)
+        entries = JournalEntry.query.filter_by(user_id=session["user_id"]).paginate(
+            page=page, per_page=per_page
+        )
+        return JournalEntrySchema(many=True).dump(entries.items), 200
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(CheckSession, '/checkSession', endpoint='checkSession')
 api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(Entries, "/entries", endpoint='entries')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
